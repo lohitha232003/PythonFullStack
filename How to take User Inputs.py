af20b29a51ceacd9996135d276da2c3c14c0b5b3
lohitha232003 <lohithachintha123@gmail.com>
2026-07-14 22:31:44 +0530

    Add files via upload

---
+++ How to take User Inputs.py
@@ -0,0 +1,40 @@
+'''a=2
+b=6
+print(a+b)'''
+
+#print(20+10)
+
+#runtime input()
+'''a=int(input("enter a value"))
+b=int(input("enter b value"))
+print(a+b)'''
+
+'''a=float(input("enter a value"))
+b=float(input("enter b value"))
+print(a+b)'''
+
+'''a=str(input("data1"))
+b=str(input("data2"))
+print(a+" "+b)'''
+
+#for string we can do 2 different types
+'''a=input("data1")
+b=input("data2")
+print(a+b)'''
+
+#examples
+
+'''fname=input("first name")
+lname=input("last name")
+print((fname+" "+lname).title())'''
+
+'''city=input("city")
+print(city)'''
+
+'''a=complex(input("enter a value"))
+b=complex(input("enter b value"))
+print(a+b)'''
+
+a=bool(input("enter value"))
+b=bool(input("enter value"))
+print(a+b)
