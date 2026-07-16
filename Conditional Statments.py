8d52cccc34335726918cf21824071aec57352267
lohitha232003 <lohithachintha123@gmail.com>
2026-07-16 23:49:48 +0530

    Add files via upload

---
+++ Conditional Statments.py
@@ -0,0 +1,284 @@
+#if using comparision operator
+'''a=10
+b=15
+if a<b:
+    print("true")'''
+
+'''a=6
+b=9
+if a>b:
+    print("false")'''#if we pass print in wrong it prints empty value
+
+'''a=12
+b=14
+if a<=b:
+    print("true")'''
+
+'''a=10
+b=15
+if a>=b:
+    print("false")'''
+
+'''a=40
+b=60
+if a!=b:
+    print("not equal")'''
+
+'''a=30
+b=30
+if a==b:
+    print("equal")'''
+
+'''a=9
+if a>3:
+    print("greater")'''#passing one input
+
+#string
+#we use only '==','!='
+'''a="python"
+if a=="python":
+    print("equal")'''
+
+'''a="python"
+if a!="java":
+    print("not equal")'''
+
+#passing user input
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<b:
+    print("less")'''
+
+'''a=int(input("a value"))
+if a>40:
+    print("greater")'''
+
+'''a=input("data")
+if a=="python":
+    print("equal")'''
+
+#if using logical operator
+'''a=5
+b=7
+if a<b and b>a:
+    print("true")'''
+
+'''a=40
+b=50
+if a<=b and b>=a:
+    print("true")'''
+
+'''a=4
+b=6
+if a!=b and b==a:
+    print("false")'''
+
+'''a=13
+b=17
+if a<b or b>a:
+    print("true")'''
+
+'''a=20
+b=30
+if a<=b or b>=a:
+    print("true")'''
+
+'''a=6
+b=12
+if a!=b or b==a:
+    print("true")'''
+
+'''a=5
+b=9
+if not a<b:
+    print("less")'''
+
+'''a=5
+b=9
+if not a>b:
+    print("less")'''
+
+'''a=5
+b=9
+if not a<b and b>a:
+    print("less")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<b and b>a:
+    print("true")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<=b and b>=a:
+    print("true")'''
+      
+#if using Identify operator
+'''a=7
+if type(a) is int:
+    print("true")'''
+
+'''a=9
+if type(a) is not int:
+    print("true")'''
+
+'''a=int(input("a value"))
+if type(a) is not int:
+    print("true")'''
+
+'''a=str(input("enter a"))
+if type(a) is str:
+    print("true")'''
+
+#if using membership operator
+
+'''a=2,3,4,5,6,7,8,9
+if 5 in a:
+    print("true")'''
+    
+'''a=2,3,4,5,6,7,8,9
+if 15 in a:
+    print("true")'''
+
+'''a=int(input("a value"))
+if 30 in a:
+    print("true")'''#error
+
+'''a=[2,3,4,5,6,7,8,9]
+b=int(input("value"))
+if b in a:
+    print("true")'''
+
+#if-else using comparision operator
+    
+'''a=3
+b=4
+if a<b:
+    print("True")
+else:
+    print("False")'''
+
+'''a=3
+b=4
+if a>b:
+    print("True")
+else:
+    print("False")'''
+
+'''a=5
+b=8
+if a<=b:
+    print("a is True")
+else:
+    print("a is False")'''
+    
+'''a=5
+b=8
+if a>=b:
+    print("a is True")
+else:
+    print("a is False")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a!=b:
+    print("true")
+else:
+    print("False")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a==b:
+    print("true")
+else:
+    print("False")'''
+
+#if-else using logical operation
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<b and b>a:
+    print("TRUE")
+else:
+    print("FALSE")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<=b and b>=a:
+    print("TRUE")
+else:
+    print("FALSE")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<b or b>a:
+    print("TRUE")
+else:
+    print("FALSE")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<=b or b>=a:
+    print("TRUE")
+else:
+    print("FALSE")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if not a>b:
+    print("TRUE")
+else:
+    print("FALSE")'''
+
+#if-else using Identity operation
+
+'''a = int(input())
+if type(a) is int:
+    print("Yes")
+else:
+    print("No")'''
+
+'''a = int(input())
+if type(a) is int:
+    print("Yes")
+else:
+    print("No")'''
+
+'''a = str(input())
+if type(a) is str:
+    print("Yes")
+else:
+    print("No")'''
+
+'''a = str(input())
+if type(a) is not str:
+    print("Yes")
+else:
+    print("No")'''
+
+#if-else using membership operation
+
+'''a = 1,2,3,4,5,6,7,8,9
+b = int(input())
+if b in a:
+    print("true")
+else:
+    print("false")'''
+
+'''a = "python","java","html","css"
+b = str(input())
+if b in a:
+    print("true")
+else:
+    print("false")'''
+
+
+'''a = "python","java","html","css"
+b = str(input())
+if b not in a:
+    print("true")
+else:
+    print("false")'''
+
+
+
